48f2462758d634386a8838914c31d29d1d97472d
ruang <longjinyii@outlook.com>
2023-09-26 20:27:52 +0800

    新增了cpu

--- cgo/cpu.c
+++ cgo/cpu.c
@@ -1,3 +1,40 @@
 #include <stdio.h>
 #include "inc/cpu.h"
 
+// CPU信息表
+// static msize_t mtable[MTableSize];
+
+// 信息名称表
+
+
+/**
+ * @brief 读取内存使用情况
+ * @param m 内存类型
+ * @return msize_t 占用字节(KB) 
+ */
+int read_mem(enum cpu_info m)
+{
+    __refresh();
+    return mtable[m];
+}
+
+/**
+ * @brief 刷新CPU资源表
+ * @param void
+ * @return int 是否执行
+ */
+static int __refresh()
+{
+    return 0;
+}
+
+
+/**
+ * @brief 查看信息表中是否存在该项
+ * @param _s 信息名
+ * @return int 是否存在
+ */
+static int __is_exists(const char *_s)
+{
+    return 0;
+}
--- cgo/inc/cpu.h
+++ cgo/inc/cpu.h
@@ -2,5 +2,16 @@
 #define _CGO_CPU_H
 
 
+enum cpu_info{
+    a = 1
+};
+
+
+int read_cpu(enum cpu_info m);
+
+static int __refresh();
+
+static int __is_exists(const char *_s);
+
 
 #endif // _CGO_CPU_H
