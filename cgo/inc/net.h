fdab6ab2d6c09e33728bbee01ada5be3d20b1df8
ruang <longjinyii@outlook.com>
2023-09-27 20:47:28 +0800

    新增net

--- cgo/inc/net.h
+++ cgo/inc/net.h
@@ -2,5 +2,12 @@
 #define _CGO_NET_H
 
 
+typedef unsigned int net_byte;
+typedef long long int byte_t;
+
+byte_t read_net_byte(net_byte n);
+
+static int __refresh();
+
 
 #endif // _CGO_NET_H
---
+++ cgo/net.c
@@ -0,0 +1,12 @@
+#include "inc/net.h"
+
+
+byte_t read_net_byte(net_byte n)
+{
+    return 0;
+}
+
+static int __refresh()
+{
+    return 0;
+}
